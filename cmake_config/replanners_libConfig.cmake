9c2dcfe3f003678f5eefc429a048d292455c93ce
Cesare Tonola <c.tonola001@unibs.it>
2024-06-24 16:20:45 +0200

    fix

--- cmake_config/replanners_libConfig.cmake
+++ cmake_config/replanners_libConfig.cmake
@@ -6,6 +6,8 @@ find_dependency(cnr_param REQUIRED)
 find_dependency(cnr_class_loader REQUIRED)
 
 if(USE_GRAPH_DISPLAY)
+    #pkg_check_modules(graph_display graph_display IMPORTED_TARGET)
+
     find_dependency(graph_display REQUIRED)
 endif()
 
