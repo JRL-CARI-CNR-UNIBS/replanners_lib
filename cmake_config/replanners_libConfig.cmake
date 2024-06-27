7b02becd35abe9c366ce5c6563d2fef89b2a849a
Cesare Tonola <c.tonola001@unibs.it>
2024-06-27 12:08:27 +0200

    fix export graph_display

--- cmake_config/replanners_libConfig.cmake
+++ cmake_config/replanners_libConfig.cmake
@@ -5,10 +5,4 @@ find_dependency(cnr_logger REQUIRED)
 find_dependency(cnr_param REQUIRED)
 find_dependency(cnr_class_loader REQUIRED)
 
-if(USE_GRAPH_DISPLAY)
-    #pkg_check_modules(graph_display graph_display IMPORTED_TARGET)
-
-    find_dependency(graph_display REQUIRED)
-endif()
-
 include("${CMAKE_CURRENT_LIST_DIR}/replanners_libTargets.cmake")
