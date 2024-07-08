44e5080d3a18d7bb9dac4c2bb90c6e0b780152da
Cesare Tonola <c.tonola001@unibs.it>
2024-07-08 17:22:09 +0200

    fixes

--- cmake_config/replanners_libConfig.cmake
+++ cmake_config/replanners_libConfig.cmake
@@ -1,3 +1,5 @@
+
+
 include(CMakeFindDependencyMacro)
 
 find_dependency(graph_core REQUIRED)
@@ -11,6 +13,7 @@ if(ON)
   find_dependency(graph_display REQUIRED)
 endif()
 
-set(replanners_lib_LIBRARIES replanners_lib)
-set(replanners_lib_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include")
-set(replanners_lib_FOUND True)
+set_and_check(replanners_lib_INCLUDE_DIRS "" ${graph_core_INCLUDE_DIRS} ${cnr_logger_INCLUDE_DIRS} ${cnr_param_INCLUDE_DIRS} ${cnr_class_loader_INCLUDE_DIRS})
+set(replanners_lib_LIBRARIES replanners_lib ${graph_core_LIBRARIES} ${cnr_logger_LIBRARIES} ${cnr_param_LIBRARIES} ${cnr_class_loader_LIBRARIES})
+
+check_required_components(replanners_lib)
