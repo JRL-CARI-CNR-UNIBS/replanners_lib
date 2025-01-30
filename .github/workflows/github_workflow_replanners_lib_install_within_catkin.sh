e20ff742d423d5e3d10ed40c719caeb11d08b179
Cesare Tonola <c.tonola001@unibs.it>
2025-01-30 09:52:14 +0100

    workflows

---
+++ .github/workflows/github_workflow_replanners_lib_install_within_catkin.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# Source ROS Noetic setup
+echo "Sourcing ROS Noetic"
+source /opt/ros/noetic/setup.bash
+
+# Define workspace paths
+WORKSPACE_DIR=$(pwd)/openmore_ws
+SRC_DIR=$WORKSPACE_DIR/src
+
+# Create the workspace and source folder
+echo "Setting up Catkin workspace at $WORKSPACE_DIR"
+mkdir -p $SRC_DIR
+cd $WORKSPACE_DIR
+
+# Clone graph_corea and replanners_lib into the src folder if not already present
+if [ ! -d "$SRC_DIR/graph_core" ]; then
+    echo "Cloning graph_core repository into $SRC_DIR"
+    git clone https://github.com/JRL-CARI-CNR-UNIBS/graph_core.git $SRC_DIR/graph_core
+
+    echo "Cloning replanners_lib repository into $SRC_DIR"
+    git clone https://github.com/JRL-CARI-CNR-UNIBS/replanners_lib.git $SRC_DIR/replanners_lib
+fi
+
+# Check for missing catkin package
+if [ ! -d "$SRC_DIR/catkin" ]; then
+    echo "Cloning catkin package into $SRC_DIR"
+    git clone https://github.com/ros/catkin.git $SRC_DIR/catkin
+fi
+
+# Build the workspace
+echo "Building the Catkin workspace"
+catkin config --extend /opt/ros/noetic
+catkin build
+
+# Source the workspace setup
+echo "Sourcing workspace setup"
+source $WORKSPACE_DIR/devel/setup.bash
---
+++ tests/src/run_all_tests.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+std::string logger_file_ = "/logger_param_gtest.yaml";
+
+void runBinary(const std::string& binary_path)
+{
+  std::cout << "Running test binary: " << binary_path << " with logger file: " << logger_file_ << "..." << std::endl;
+
+  std::string command = binary_path + " " + logger_file_;
+
+  int result = std::system(command.c_str());
+  ASSERT_EQ(result, 0) << "Test binary " << binary_path << " failed with exit code " << result;
+}
+
+TEST(TestSuite, PathReplanningTest)
+{
+  runBinary("./path_replanning_test");
+}
+
+int main(int argc, char** argv)
+{
+  if (argc > 1)
+    logger_file_ = std::string(argv[1]);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
